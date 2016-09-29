f6ad9ba741b8ee36ba4b9bab536620e324e8a926
Yakup Ates <Yakup.Ates@rub.de>
2016-09-29 16:40:45 +0200

    We need non-static functions here.

--- 0_view/form.h
+++ 0_view/form.h
@@ -25,32 +25,32 @@ class Form{
  private:
 	const char* label_default = "DEFAULT";
 	int hbox_border = 5;
-	int hbox_border = 0;
+	int vbox_border = 0;
 	
-	GtkWidget *hbox = gtk_hbox_new(FALSE, hbox_border);
-	GtkWidget *vbox = gtk_vbox_new(FALSE, vbox_border);
-	GtkWidget *label;
-	GtkWidget *txtField;
-	GtkWidget *checkbox;
+	GtkWidget* hbox = gtk_hbox_new(FALSE, hbox_border);
+	GtkWidget* vbox = gtk_vbox_new(FALSE, vbox_border);
+	GtkWidget* label;
+	GtkWidget* txtField;
+	GtkWidget* checkbox;
 
  public:
 	Form();
 	Form(const char *label);
 	
-	static GtkWidget* get_hbox();
-	static GtkWidget* get_vbox();
-	static GtkWidget* get_label();
-	static GtkWidget* get_txtField();
-	static GtkWidget* get_checkbox();
-	static GtkWidget* set_hbox();
-	static GtkWidget* set_vbox();
-	static GtkWidget* set_label();
-	static GtkWidget* set_txtField();
-	static GtkWidget* set_checkbox();
-
-	static GtkWidget *create_checkbox(const char *label);
-	static GtkWidget *create_label(const char *txt_label);
-	static GtkWidget *create_txtField();
+	 GtkWidget* get_hbox();
+	 GtkWidget* get_vbox();
+	 GtkWidget* get_label();
+	 GtkWidget* get_txtField();
+	 GtkWidget* get_checkbox();
+	 void set_label(GtkWidget* label_widget);
+	 void set_txtField(GtkWidget* txtField_widget);
+	 void set_checkbox(GtkWidget* checkbox_widget);
+	 void set_hbox(GtkWidget* hbox_widget);
+	 void set_vbox(GtkWidget* vbox_widget);
+	 
+	 GtkWidget* create_checkbox(const char* label);
+	 GtkWidget* create_label(const char* txt_label);
+	 GtkWidget* create_txtField();
 
 };
 
